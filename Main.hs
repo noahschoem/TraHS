31f9fecb53e81031bf52123f9f94a9d0620dc139
Noah Schoem <nschoem@gmail.com>
2015-06-03 15:20:02 -0500

    More thinking about how to do this.

--- Main.hs
+++ Main.hs
@@ -9,33 +9,40 @@ import Trahs
 
 main :: IO ()
 main = do
+  -- performs a one-way sync from dir1 to dir2
   -- read contents of both directories
   [dir1,dir2] <- getArgs  
 -- read the corresponding .trahs.db files.
 -- If one doesn't exist, initialize it.
   (tradb1,files1) <- readTraDB dir1
   (tradb2,files2) <- readTraDB dir2
-  trahs (read tradb1) files1 (read tradb2) files2
+  trahs (read tradb1) (files1,dir1) (read tradb2) (files2,dir2)
     
 
 -- Now perform trahs.
 -- readTraDB :: String -> IO (String,String)
 readTraDB dir = do
   dirContents <- getDirectoryContents dir
-  unless (elem ".trahs.db" dirContents) (createNewTRADB dir)
+  unless (elem ".trahs.db" dirContents) (createNewTRADB dir dirContents)
   tradb <- readFile (dir ++ ".trahs.db")
-  let dirContents' =  filter (\x -> x /= "." && x /= ".." && x /= ".trahs.db") dirContents
+  let dirContents' = removeJunk dirContents
   return (tradb,dirContents')
   
 -- | createNewTRADB: instantiates a .trahs.db file at a given directory 
 -- if none exists yet.
 -- TODO: finish writing this
-createNewTRADB dir = do
+createNewTRADB dir dirContents = do
   uid <- genUID
-  let fileList = "[]" -- will have to change
-  let newdb = "1\n" ++ uid ++ "\n"
+  let fileList = removeJunk dirContents
+  let fileMap = 
+  let newdb = TraDB {lvn = 1,
+                     replicaID = uid,
+                     fileData = []} -- this needs to change
   writeFile (dir ++ ".trahs.db") $ show newdb
        
+removeJunk :: [FilePath] -> [FilePath]
+removeJunk = filter (\x -> x /= "." && x /= ".." && x /= ".trahs.db")
+
 -- needs to change
 genUID :: IO String
 genUID = return ""
--- src/Trahs.hs
+++ src/Trahs.hs
@@ -4,16 +4,37 @@ import Codec.Digest.SHA
 import qualified Data.Map as M
 import Data.ByteString
 
-trahs :: TraDB -> [String] -> TraDB -> [String] -> IO ()
-trahs db1 files1 db2 files2 = do
-  return ()
+-- | trahs: performs a unidirectional sync from dir1 to dir2.
+--   Gets passed in arguments for the database info
+--   and file list for dir1 and dir2.
+trahs :: TraDB -> ([String],String) -> TraDB -> ([String],String) -> IO ()
+trahs db1 (files1,dir1) db2 (files2,dir2) = forM_ files1 $ \file -> do
+  -- stage 1: checks for any modifications to files in directories 1 and 2
+  newdb1 <- checkForMods db1 files1 dir1
+  newdb2 <- checkForMods db2 files2 dir2
+  -- stage 2: now, sync directories 1 and 2
+  
+  -- finally, write the databases to the target folders
+  
+  where
+    checkForUpdates 
 
-data FileData = FileData {lastHash :: ByteString,
+{- | FileData: represents the relevant file info for each file
+     filename: the name of the file
+     lastHash: the SHA256 hash of the last version
+     versionVector: a version vector for the current file
+-}
+data FileData = FileData {filename :: String,
+                          lastHash :: ByteString,
                           versionVector :: M.Map String Integer}
   deriving(Eq,Read,Show)
 
+{- | TraDB: represents a database for tra.
+     lvn: local version number
+     replicaID: the unique identifier of the local directory
+     fileData: a list of FileData, one for each file
+-}
 data TraDB = TraDB {lvn :: Integer, 
                     replicaID :: String, 
                     fileData :: [FileData]}
   deriving(Eq,Read,Show)
--- should probably make version vectors as maps
